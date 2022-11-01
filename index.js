35049b32040467a9f75e2704b26fbc4b4279b6ef
Hoang Tran <jashoang2583@gmail.com>
2022-11-01 08:46:16 -0700

    commit tree 7d051cb659934f56710eea4d5b8294ecd6d56a68

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+const newCat = "Broom"
+function appendCat(name) {
+    return cats.append(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+const removeFirstClass = () => {
+    return cats.slice(-1)
+}
